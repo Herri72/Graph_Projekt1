5e59f59b28f2a29a9baf36ba17ca516daea51764
Herri72 <142775749+Herri72@users.noreply.github.com>
2025-05-23 12:21:31 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "parameter_reader.h"
+#include <iostream>
+
+
+using namespace std; // wuerde ich empfehlen zu benutzen, diese Zeile kann aber auch geloescht werden
+// Nach dem Loeschen dieser Zeile muss unten dann allerdings std::vector<std::string> geschrieben werden!
+
+int main(int argc, char** argv) {
+
+
+
+    vector<string> input_vector = input_to_vector(argc, argv); // the input given in console is now in input_vector
+    // (e.g. for input "hallo 1 a b" input_vector is {"hallo", "1", "a", "b"})
+
+
+    //extend this code
+}
+
+bool bfs(int i){
+
+
+    return false;
+}
